072b4462a23c8724274955a9a53161b2fc9109f0
Bruno Vidal <brunokasprik@gmail.com>
2023-11-03 00:57:58 -0300

    exercise 2 correction

--- project-toast/src/components/Toast/Toast.js
+++ project-toast/src/components/Toast/Toast.js
@@ -13,17 +13,18 @@ import VisuallyHidden from '../VisuallyHidden';
 import styles from './Toast.module.css';
 
 const ICONS_BY_VARIANT = {
-  notice: <Info/>,
-  warning: <AlertTriangle/>,
-  success: < CheckCircle />,
-  error: < AlertOctagon />,
+  notice: Info,
+  warning: AlertTriangle,
+  success:  CheckCircle ,
+  error: AlertOctagon ,
 };
 
-function Toast({message,variant,setShowToast}) {
+function Toast({ message, variant, setShowToast }) {
+  const Icon = ICONS_BY_VARIANT[variant]
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        {variant && ICONS_BY_VARIANT[variant]}
+        <Icon/>
       </div>
       <p className={styles.content}>
         {message}
